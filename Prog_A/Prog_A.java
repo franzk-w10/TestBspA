cb2c1685ac399c23ff716cf5cc7bd1193978c9f8
kranzfra <kranzfra@EDV3-01.htlw10.at>
2015-06-02 13:39:53 +0200

    Start Version

---
+++ Prog_A/Prog_A.java
@@ -0,0 +1,15 @@
+
+public class Prog_A {
+
+	public static void main(String[] args) {
+
+		verabschiedung();
+
+	}
+
+	//Ausgabe der Verabschiedung
+	static void verabschiedung() {
+			
+		System.out.println("\n\n\t Have A Nice Day!");
+	}	
+}
