9c25a6641ba7c07fc7894340b2fa27f0d3c8a978
kranzfra <kranzfra@EDV3-01.htlw10.at>
2015-06-02 13:54:02 +0200

    Erweiterung 1

--- Prog_A/Prog_A.java
+++ Prog_A/Prog_A.java
@@ -1,12 +1,30 @@
-
+import AlgoTools.IO;
 public class Prog_A {
 
 	public static void main(String[] args) {
 
+		titel("Faktorielle - Tabelle");
+		
 		verabschiedung();
 
 	}
 
+	//Titelausgabe
+	static void titel(String text) {
+			
+		System.out.print("\n\t\t");
+		
+		for (int i=0; i<text.length()+6; i++)
+			System.out.print("*");
+			
+		System.out.print("\n\t\t*  "+ text + "  *\n\t\t");
+			
+		for (int i=0; i<text.length()+6; i++)
+			System.out.print("*");
+			
+		System.out.println("\n");
+	}
+	
 	//Ausgabe der Verabschiedung
 	static void verabschiedung() {
 			
